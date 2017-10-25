02ca88eb173e7df998d8630a17424dc5cd8fd985
survivechris <survival_of_chris@hotmail.com>
2017-10-25 15:58:44 +0800

    add custom registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,50 @@
+class RegistrationsController < Devise::RegistrationsController
+  # extend the Devise Controller and refer to Devise gem file
+  def create
+    build_resource(sign_up_params)
+
+    resource.class.transaction do
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        # some additional custom modifications to extend devise gem
+        @payment = Payment.new({ email: params['user']['email'],
+                                 token: params[:payment]['token'],
+                                 user_id: resource.id })
+        flash[:error] = "Please check registration errors" unless @payment.valid?
+        begin
+          @payment.process_payment
+          @payment.save
+        rescue Exception => e
+          flash[:error] = e.message
+          resource.destroy
+          puts 'Payment failed'
+          render :new && return
+        end
+
+        # the same process as devise gem file defined
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    # how about this??
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:payment])
+    devise_parameter_sanitizer.for(:sign_up).push(:payment)
+  end
+end
